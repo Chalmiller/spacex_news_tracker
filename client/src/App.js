ef33c6b42bb26595751221b9cfb59aca8d30baa2
Chalmiller <christopher@transcriptic.com>
2020-02-09 15:26:35 -0800

    Deploying app to heroku

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.png';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Launches from './components/Launches';
+import Launch from './components/Launch';
+import './App.css';
+
+const client = new ApolloClient({
+  uri: '/graphql'
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <img src={logo} alt="Spacex" style={{ width: 300, display: 'block', margin: 'auto' }}></img>
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ public/precache-manifest.669fffae0ee4693997d4f340cb53ee1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8d5fd7b5740c832e8638a7e97e3c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48329f9e4d871dd971dd",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ef630c2258bb8fda329b",
+    "url": "/static/js/2.96fe9346.chunk.js"
+  },
+  {
+    "revision": "a0c18f8786874a74d6fd4d46260562f6",
+    "url": "/static/js/2.96fe9346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48329f9e4d871dd971dd",
+    "url": "/static/js/main.7054fae6.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "f98a21db746bc024b64ac3c5ac7fae63",
+    "url": "/static/media/logo.f98a21db.png"
+  }
+]);
